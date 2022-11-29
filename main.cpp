4e32bc3b37ebb332f724a517299d0071d08c6cab
mrxYan6 <1827732507@qq.com>
2022-11-29 21:43:15 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <functional>
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <array>
+#include <map>
+
+using i64 = long long;
+
+void menu() {
+	std::cout << "---------------------------------" << std::endl;
+
+}
+
+int main() {
+
+}
